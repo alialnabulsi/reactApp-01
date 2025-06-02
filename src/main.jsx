e85e4fc281fad7b59bc0ab3f48f9691493df9b88
alialnabulsi <ali.alnabulsi@std.balamand.edu.lb>
2025-06-02 23:01:34 +0300

    v.1.0

---
+++ src/main.jsx
@@ -0,0 +1,17 @@
+import {createRoot} from 'react-dom/client';
+const root = createRoot(document.getElementById('root'));
+import './main.css';
+
+root.render(
+  <main>
+        <img className='react-logo' src="src/assets/react.png" alt="React logo" />
+        <h1>Fun facts about React!</h1>
+        <ul>
+            <li>Was first release in 2013</li>
+            <li>Was originally created by Jordan Walke</li>
+            <li>Has well over 200K stars on GitHub</li>
+            <li>Is maintained by Meta</li>
+            <li>Powers thousands of enterprise apps, including mobile apps</li>
+        </ul>
+    </main>
+)
